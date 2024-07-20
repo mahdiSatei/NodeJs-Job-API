b784fc97a670078bbd82b0d4685c6ea55a49e535
mahdi <mahdisatei82@gmail.com>
2024-07-20 11:30:33 +0330

    setup pre function to hash the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const User = require('../models/User')
+const bcyrpt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     const user = await User.create(req.body)
+    
     res.json({user})
 }
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcyrpt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema ({
     name : {
@@ -23,5 +24,10 @@ const UserSchema = new mongoose.Schema ({
     }
 })
 
+UserSchema.pre('save', async function(next){
+    const salt = await bcyrpt.genSalt(10)
+    this.password = await bcyrpt.hash(this.password, salt)
+    next()
+})
 // module.exports = mongoose.model('User', UserSchema)
 module.exports = mongoose.model('User', UserSchema)
