e72150a02c5a8ea56c529d00b4834c6f346be741
mahdi <mahdisatei82@gmail.com>
2024-07-20 10:30:28 +0330

    setup connectDB and connect the app to the database

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 // routes
 const authRouter = require('./routes/auth')
 
+// connect datebase
+const connectDB = require('./db/connect')
+
 app.use('/auth', authRouter)
 app.use(express.json())
 
@@ -17,8 +20,9 @@ app.use(errorHandlerMiddleware)
 
 const PORT = process.env.PORT || 3000
 
-const start = () => {
+const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(PORT, ()=> console.log(`Server is listening to port ${PORT}`))
     } catch (error) {
         console.log(error);
---
+++ db/connect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+
+const connectDB = (uri) => {
+    return mongoose.connect(uri)
+}
+
+module.exports = connectDB
