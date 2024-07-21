b7bc4decd82b82dae0bcf068fe2c89d4564309fd
mahdi <mahdisatei82@gmail.com>
2024-07-21 13:36:40 +0330

    refactor the error handler to handl cast and other errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,30 @@
-const { CustomErrorAPI } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (error, req, res, next) => {
-    if (error instanceof CustomErrorAPI) {
-       return res.status(error.statusCode).json({msg : error.message})
+
+    let customError = {
+        statusCode : error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg : error.message || "Something went wrong, please try again later"
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+
+    if (error.code && error.code === 11000) {
+        customError.statusCode = StatusCodes.BAD_REQUEST
+        customError.msg = "User already exist"
+    }
+    
+    if (error.name === "ValidationError") {
+        customError.msg = Object.values(error.errors)
+            .map((item) => item.message)
+            .join(', ')
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    if (error.name === "CastError") {
+        customError.msg = `No item found with id ${error.value}`
+        customError.statusCode = StatusCodes.NOT_FOUND
+    }
+
+    return res.status(customError.statusCode).json({msg : customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
