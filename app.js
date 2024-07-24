067ae820bafa3b9199032759df057b5e8a1b246a
mahdi <mahdisatei82@gmail.com>
2024-07-24 18:56:18 +0330

    add security packages and use them

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 const app = express()
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const rateLimeter = require('express-rate-limit')
+
 
 // middleware 
 const notFoundMiddleware = require('./middleware/not-found')
@@ -15,7 +20,17 @@ const jobRouter = require('./routes/job')
 // connect datebase
 const connectDB = require('./db/connect')
 
+app.use(rateLimeter({
+    windowMs : 15 * 60 * 100,
+    limit : 100
+}))
+
 app.use(express.json())
+app.use(cors())
+app.use(xss())
+app.use(helmet())
+
+// router
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authMiddelware, jobRouter)
 
